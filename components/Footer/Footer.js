0a01e7415550b162a4f47869b3e8bc385aaed05d
JC <jc.castagne96@gmail.com>
2023-03-21 11:32:47 -0400

    Complete footer.

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,50 @@
 import './Footer.module.css'
+import links from './links'
+
+import { Box, Link, Heading, Text, VStack, HStack } from '@chakra-ui/react'
 
 export default function Footer () {
-  return <div class='Footer'>Footer</div>
+  return (
+    <HStack
+      id='Navbar'
+      justifyContent='center'
+      alignItems='center'
+      width='100vw'
+      backgroundColor='teal'
+      height='100%'
+    >
+      <HStack
+        id='container'
+        justifyContent='space-between'
+        alignItems='center'
+        width='100%'
+        maxWidth={1080}
+        paddingX={2}
+        paddingY={16}
+        color='white'
+        height='100%'
+      >
+        <Box display='grid' gridTemplateColumns='1fr 1fr' width='100%'>
+          {links &&
+            links.map((item, index) => {
+              return (
+                <Link href={`${item?.url}`} key={index * 99} paddingBottom={2}>
+                  {item?.title}
+                </Link>
+              )
+            })}
+        </Box>
+
+        <VStack height='80px' alignItems='start' justifyContent='space-between'>
+          <Heading fontSize='2xl' justifyContent='flex-start'>
+            LOGO.
+          </Heading>
+
+          <Text opacity='0.66' width='max-content'>
+            JC Castagne &copy; 2023
+          </Text>
+        </VStack>
+      </HStack>
+    </HStack>
+  )
 }
---
+++ components/Footer/links.js
@@ -0,0 +1,28 @@
+const links = [
+  {
+    title: 'Home',
+    url: '/'
+  },
+  {
+    title: 'About',
+    url: '/about'
+  },
+  {
+    title: 'Contact',
+    url: '/contact'
+  },
+  {
+    title: 'Social',
+    url: '/social'
+  },
+  {
+    title: 'Support',
+    url: '/support'
+  },
+  {
+    title: 'FAQ',
+    url: '/faq'
+  }
+]
+
+export default links
