6f479c86c2a7d29020c0986bb9e7991cdf3e0069
JC <jc.castagne96@gmail.com>
2023-03-21 10:28:52 -0400

    Complete default global layout that will contain all pages.

--- layouts/DefaultLayout/DefaultLayout.js
+++ layouts/DefaultLayout/DefaultLayout.js
@@ -1,13 +1,24 @@
 import './DefaultLayout.module.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import { Box } from '@chakra-ui/react'
 
 export default function DefaultLayout ({ children }) {
   return (
-    <div class='DefaultLayout'>
+    <Box
+      id='DefaultLayout'
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='space-between'
+      minWidth='100vw'
+      minHeight='100vh'
+    >
       <Navbar />
-      <main>{children && children}</main>
+      <Box id='DefaultPageLayout'>
+        <main>{children && children}</main>
+      </Box>
       <Footer />
-    </div>
+    </Box>
   )
 }
