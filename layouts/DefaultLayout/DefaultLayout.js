c6e163de5b6ce81c7a3127d1000ddcffbf589780
JC <jc.castagne96@gmail.com>
2023-03-21 20:13:07 -0400

    Fix height values.

--- layouts/DefaultLayout/DefaultLayout.js
+++ layouts/DefaultLayout/DefaultLayout.js
@@ -1,9 +1,10 @@
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
-import { Box } from '@chakra-ui/react'
+import { Box, useColorMode, VStack } from '@chakra-ui/react'
 import ColorModeButton from '../../components/ColorModeButton/ColorModeButton'
 
 export default function DefaultLayout ({ children }) {
+  const { colorMode } = useColorMode()
   return (
     <Box
       id='DefaultLayout'
@@ -12,12 +13,16 @@ export default function DefaultLayout ({ children }) {
       alignItems='center'
       justifyContent='space-between'
       minWidth='100vw'
-      minHeight='100vh'
+      minHeight='calc(100vh - 72px)'
+      color={colorMode === 'dark' ? 'whitesmoke' : '#344949'}
     >
       <Navbar />
-      <Box id='DefaultPageLayout'>
-        <main>{children && children}</main>
-      </Box>
+
+      <VStack id='DefaultPageLayout' width='100%' height='100%'>
+        <Box id='container' maxWidth={1080} paddingX={8} paddingY={4}>
+          <main>{children && children}</main>
+        </Box>
+      </VStack>
       <Footer />
 
       <ColorModeButton />
