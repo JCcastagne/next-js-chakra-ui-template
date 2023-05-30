b9c510af3d7f88e0556b4ca7e3c8c61fd219f104
JC <jc.castagne96@gmail.com>
2023-05-30 00:27:52 -0400

    Update background colors.

--- layouts/RootLayout/RootLayout.js
+++ layouts/RootLayout/RootLayout.js
@@ -14,10 +14,16 @@ export default function RootLayout ({ children }) {
       minWidth='100vw'
       minHeight='calc(100vh - 72px)'
       color={colorMode === 'dark' ? 'whitesmoke' : '#344949'}
+      backgroundColor={colorMode === 'dark' ? 'gray.800' : ''}
     >
       <Navbar />
 
-      <VStack id='DefaultPageLayout' width='100%' height='100%'>
+      <VStack
+        id='DefaultPageLayout'
+        width='100%'
+        height='100%'
+        minHeight={'calc(110vh - 114px)'}
+      >
         <Box id='container' maxWidth={1080} paddingX={8} paddingY={4}>
           <main>{children && children}</main>
         </Box>
